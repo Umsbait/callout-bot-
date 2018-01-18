7a1d18ae97e13e95dba34563e8e1ff6d6fc0b8f2
Umsbait <umsbaav@gmail.com>
2018-01-18 12:55:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/; botRegexSlut = /^\/slut/;
+      botRegex = /^\/form/; botRegexSlut = /^\/slut/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -13,7 +13,7 @@ function respond() {
                 "MIA","BUF","SF","WAS","NYJ","TB"]
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(cool());
+    postMessage("https://docs.google.com/forms/d/e/1FAIpQLSfISy5hg61KoVdpa1jLeqvmP7g5ccO1BpYZIJbxxnr-3sivpA/viewform");
     this.res.end();
   }
   else if(request.text && botRegexSlut.test(request.text)) {
